d94e05b6ac92bca325b6eb721e7526835924d725
BY571 <sebastian.dittert@gmx.de>
2023-11-21 15:33:54 +0100

    fix disconnect issue and add verbose mode for env and run script

--- environments/base/base_env.py
+++ environments/base/base_env.py
@@ -19,7 +19,9 @@ class BaseEnv(Env):
         self,
         action_dim: int,
         state_dim: int,
+        verbose: bool = False,
     ):
+        self.verbose = verbose
         self.action_dim = action_dim
         self.state_dim = state_dim
 
@@ -61,9 +63,11 @@ class BaseEnv(Env):
             np.array: The current state of the environment as a numpy array.
         """
         byte_state = self.hub.read()
-        print("Reading data size: ", sys.getsizeof(byte_state))
-        print("Reading data: ", byte_state)
-        print("len: ", len(byte_state))
+        if self.verbose:
+            print("BaseEnv read_from_hub: ")
+            print("Reading data size: ", sys.getsizeof(byte_state))
+            print("Reading data: ", byte_state)
+            print("Byte len: ", len(byte_state))
         # assert sys.getsizeof(byte_state) == 53, f"State has size {sys.getsizeof(byte_state)} but should have size 53."
         if len(byte_state) != self.expected_bytesize:
             print(
